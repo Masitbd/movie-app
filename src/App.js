42a753c9c9b4af582d41540fb4090bf62c3ea115
sarker24 <sarker24@gmail.com>
2022-11-02 20:05:10 +0100

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
-import './App.css';
+
 import Navbar from './components/navbar/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import MovieGrid from '../components/grid/MovieGrid';
+import Navbar from '../components/navbar/Navbar';
+import Tags from '../components/tags/Tags';
+import Footer from '../ui/Footer';
+import Pagination from '../ui/Pagination';
+
+const Home = () => {
+    return (
+        <>
+          <Navbar />
+          <Tags />
+          <MovieGrid />
+          <Pagination />
+          <Footer />  
+        </>
+    );
+};
+
+export default Home;
