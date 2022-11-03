406eaead3c75ba6cc9746e5f17d31d7dc594c174
sarker24 <sarker24@gmail.com>
2022-11-03 10:51:22 +0100

    Add the realted video and nav

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
-
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/Home';
+import Movie from './pages/Movie';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Movie />
     </div>
   );
 }
---
+++ src/list/RelatedMovieList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import RelatedMovie from './RelatedMovie';
+
+const RelatedMovieList = () => {
+    return (
+        <div
+        class="col-span-full lg:col-auto max-h-[570px] overflow-y-auto"
+    >
+        <RelatedMovie />
+        
+    </div>
+
+    );
+};
+
+export default RelatedMovieList;
